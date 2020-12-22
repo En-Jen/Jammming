adf48be629ca26a3b13187152fd4f9d387759dd8
Jen Speak <jspeak6@gmail.com>
2020-12-22 14:24:12 -0800

    implemented Spotify search request

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,8 +1,8 @@
-const accessToken;
 const clientId = 'c3deb2f31242424783f0f651e83cf477';
 const redirectUri = 'http://localhost:3000/';
+let accessToken;
 
-const Spotify = {
+export const Spotify = {
     getAccessToken() {
         if (accessToken) {
             return accessToken;
@@ -21,10 +21,29 @@ const Spotify = {
             window.history.pushState('Access Token', null, '/');
             return accessToken;
         } else {
-            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
-            window.location = accessUrl;
+            window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
         }
+    },
+
+    search(term) {
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (!jsonResponse.tracks) {
+                return [];
+            }
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artists[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }));
+        });
     }
 };
-
-export default Spotify;
